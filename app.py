79ded2e17efcf557d1f9e0a0715c94dd7404d899
Emre Baddal <emrebaddal@192.168.1.8>
2026-07-28 00:59:04 +0300

    grafik eklendiv15

--- app.py
+++ app.py
@@ -631,19 +631,25 @@ with main_tab1:
             " alınamadı. Lütfen 1-2 dakika bekleyip sayfayı yenileyin."
         )
 
-    with sub_tab_chart:
-      st.subheader(f"📉 {selected_stock} - TradingView Canlı Teknik Grafiği")
-      tv_symbol = get_tradingview_symbol(selected_stock)
-      
-      theme_mode = st.radio("Grafik Modu:", ["Koyu Mod (Dark)", "Beyaz Mod (Light)"], horizontal=True, key="tv_theme_radio")
-      t_theme = "light" if "Beyaz" in theme_mode else "dark"
+with sub_tab_chart:
+  st.subheader(f"📉 {selected_stock} - TradingView Canlı Teknik Grafiği")
+  tv_symbol = get_tradingview_symbol(selected_stock)
 
-      tv_html = f"""
+  theme_mode = st.radio(
+      "Grafik Modu:",
+      ["Koyu Mod (Dark)", "Beyaz Mod (Light)"],
+      horizontal=True,
+      key="tv_theme_radio",
+  )
+  t_theme = "light" if "Beyaz" in theme_mode else "dark"
+
+  # hidesidetoolbar=1 yapılarak arama/hisse değiştirme çubuğu kapatıldı ve hata engellendi
+  tv_html = f"""
       <div class="tradingview-widget-container" style="height:600px;width:100%">
-        <iframe scrolling="no" allowtransparency="true" frameborder="0" sandbox="allow-scripts allow-same-origin allow-popups" src="https://s.tradingview.com/widgetembed/?symbol={tv_symbol}&interval=D&hidesidetoolbar=0&symboledit=1&saveimage=1&toolbarbg=f1f3f6&studies=[]&theme={t_theme}&style=1&timezone=Europe%2FIstanbul&locale=tr" style="height:100%;width:100%;"></iframe>
+        <iframe scrolling="no" allowtransparency="true" frameborder="0" sandbox="allow-scripts allow-same-origin allow-popups" src="https://s.tradingview.com/widgetembed/?symbol={tv_symbol}&interval=D&hidesidetoolbar=1&symboledit=0&saveimage=1&toolbarbg=f1f3f6&studies=[]&theme={t_theme}&style=1&timezone=Europe%2FIstanbul&locale=tr" style="height:100%;width:100%;"></iframe>
       </div>
       """
-      st.components.v1.html(tv_html, height=620)
+  st.components.v1.html(tv_html, height=620)
 
     with sub_tab2:
       if selected_stock.endswith(".IS"):
